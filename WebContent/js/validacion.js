15c570630fd8b590ea309d246f715e72259b55cf
gabo32 <j.gabriel.b@live.com.mx>
2017-03-19 23:16:34 -0600

    Inicio Capitulo 2

--- WebContent/js/validacion.js
+++ WebContent/js/validacion.js
@@ -2,12 +2,14 @@
  * 
  */
 function validacion() {
-		var isbn = document.getElementById("isbn");
-		var miformulario = document.getElementById("miformulario");
-		if (isbn.value == "") {
-			alert("datos no validos");
-			return false;
-		} else {
-			miformulario.submit();
-		}
+	// uso de DOM standard a nivel de javascript 
+	var isbn= document.getElementById("isbn");
+	var miformulario=document.getElementById("formularioInsercion");
+	
+	if(isbn.value==""){
+		alert("datos no validos");
+		return false;
+	}else{
+		miformulario.submit();
 	}
+}
